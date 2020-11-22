078f00c81a58f4c83b7dbece16fa43be0bceb11d
Deepan M <55130173+deepanmurugan@users.noreply.github.com>
2020-11-22 22:26:20 +0530

    Update gitcheckout.groovy

--- vars/gitcheckout.groovy
+++ vars/gitcheckout.groovy
@@ -1,3 +1,3 @@
-def checkout(Map stageParams){
+def call(Map stageParams){
   checkout([$class: 'GitSCM', branches: [[name: stageParams.branch]], userRemoteConfigs: [[credentialsId: 'github_repo', url: stageParams.repoUrl]]])
 }
