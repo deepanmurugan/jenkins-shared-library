31b23f2538a16b61550205a92da15042fe487e78
Deepan M <55130173+deepanmurugan@users.noreply.github.com>
2020-11-22 21:12:06 +0530

    Update gitcheckout.groovy

--- vars/gitcheckout.groovy
+++ vars/gitcheckout.groovy
@@ -1,4 +1,3 @@
 def checkout(Map stageParams){
-  dir('/tmp/ansible-playbooks/')
   checkout([$class: 'GitSCM', branches: [[name: stageParams.branch]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github_repo', url: stageParams.repoUrl]]])
 }
