e1591bffa443f93d877900998ee80edb3421dd8d
Deepan M <55130173+deepanmurugan@users.noreply.github.com>
2020-11-22 23:15:22 +0530

    Update imagepush.groovy

--- vars/imagepush.groovy
+++ vars/imagepush.groovy
@@ -1,6 +1,9 @@
-def call(String MydockerImage) {
-  echo "${MydockerImage}"
-  //docker.withRegistry( '', registryCredential ) {
-  //  dockerImage.push()
-  //}
+def call(String dockerImage) {
+  echo "${dockerImage}"
+  withCredentials([usernamePassword(credentialsId: 'dockerhub', usernameVariable: 'hubUsername', passwordVariable: 'hubPassword')]) {
+        sh """
+            docker login --username="${hubUsername}" --password="${hubPassword}"
+            docker push "${dockerImage}"
+        """
+    }
 }
